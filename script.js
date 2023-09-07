94d647b443e36e46e04fce6ac42def59029b846b
Ajmal777 <81432526+Ajmal777@users.noreply.github.com>
2023-09-07 19:09:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-//your code here
+const name = document.getElementById('item-name-input');
+const price = document.getElementById('item-price-input');
+const btn = document.getElementById('add');
+const tbody = document.getElementById('table-tbody');
+const total = document.getElementById('total');
+let totalCost = 0;
+btn.addEventListener('click', () =>{
+	const itemName = name.value;
+	const itemPrice = Number(price.value);
+	totalCost += itemPrice;
+	name.value = '';
+	btn.value = '';
+	const tr = document.createElement('tr');
+	tr.innerHTML = `
+	<td>${itemName}</td>
+	<td>${itemPrice}</td>
+	`
+	tbody.appendChild(tr);
+	total.innerText = totalCost;
+})
